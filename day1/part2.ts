e5c7f252343d94c764cbf0a0017ff153840dc3f8
kp <iamkahvi@kps-MacBook-Air.local>
2023-12-02 01:54:53 -0800

    day1pt2 not working yet

--- day1/part2.ts
+++ day1/part2.ts
@@ -1,4 +1,4 @@
-const text = await Deno.readTextFile("./test1.txt");
+const text = await Deno.readTextFile("./input2.txt");
 
 const digits = {
   "one": 1,
@@ -10,33 +10,62 @@ const digits = {
   "seven": 7,
   "eight": 8,
   "nine": 9,
-}
+};
 
-const replacedText = Object.keys(digits).reduce((acc, d) => {
-  return acc.replaceAll(d, digits[d])
-}, text)
+const digitStrings = Object.keys(digits);
 
-console.log(replacedText);
+let regex = /(one|two|three|four|five|six|seven|eight|nine)/;
 
+// const replacedText = Object.keys(digits).reduce((acc, d) => {
+//   return acc.replaceAll(d, digits[d])
+// }, text)
 
-const lines = replacedText.split("\n").filter((line) => Boolean(line));
+// console.log(replacedText);
 
+const lines = text.split("\n").filter((line) => Boolean(line));
+
+// eig      []
+// eigh     []
+// eight    []
+// 8        [8]
+// 8w       [8]
+// 8wo      [8]
+// ...      [8]
+// 8wothree [8]
+// 8wo3     [8,3]
 
 const calibrationValues = lines.map((line) => {
+  let numsRes: number[] = [];
 
-  const nums = line.replace(/\D/g,'');
+  console.log(`\n--${line}--\n`);
 
-  if (nums.length === 0) {
-    return 0;
+  let j = 0;
+  for (let i = 0; i < line.length; i++) {
+    const lineSegment = line.substring(j, i + 1);
+    console.log(lineSegment);
+
+    if (/\d/.test(lineSegment)) {
+      const digit = lineSegment.match(/\d/g);
+      numsRes.push(parseInt(digit));
+      j = i + 1;
+    } else if (regex.test(lineSegment)) {
+      digitStrings.forEach((digitString) => {
+        if (lineSegment.includes(digitString)) {
+          numsRes.push(digits[digitString]);
+          j = i + 1;
+        }
+      });
+    }
+    console.log(numsRes);
   }
 
-  if (nums.length === 1) {
-    return parseInt(nums[0] + nums[0])
-  }
+  if (numsRes.length === 0) return 0;
 
-  return parseInt(nums[0] + nums[nums.length - 1])
-})
+  if (numsRes.length === 1) return (numsRes[0] * 10 + numsRes[0]);
+
+  return (numsRes[0] * 10 + numsRes[numsRes.length - 1]);
+});
 
 console.log(calibrationValues);
 
-console.log(calibrationValues.reduce((acc, x) => acc + x, 0))
+console.log(calibrationValues.reduce((acc, x) => acc + x, 0));
