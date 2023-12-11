ff2a4ab5bea3d9129f78cfddb35c4b7ef09561cf
kp <iamkahvi@kps-MacBook-Air.local>
2023-12-10 23:18:33 -0800

    nice day7 looking good

--- day7/part1/src/main.rs
+++ day7/part1/src/main.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
 #[derive(Debug)]
 enum Card {
     A = 14,
@@ -15,6 +18,27 @@ enum Card {
     Two = 2,
 }
 
+impl Card {
+    fn from_char(c: &char) -> Option<Card> {
+        match c {
+            'A' => Some(Card::A),
+            'K' => Some(Card::K),
+            'Q' => Some(Card::Q),
+            'J' => Some(Card::J),
+            'T' => Some(Card::T),
+            '9' => Some(Card::Nine),
+            '8' => Some(Card::Eight),
+            '7' => Some(Card::Seven),
+            '6' => Some(Card::Six),
+            '5' => Some(Card::Five),
+            '4' => Some(Card::Four),
+            '3' => Some(Card::Three),
+            '2' => Some(Card::Two),
+            _ => None,
+        }
+    }
+}
+
 #[derive(Debug)]
 enum HandType {
     Five(Card),
@@ -26,6 +50,99 @@ enum HandType {
     High(Card),
 }
 
+#[derive(Debug)]
+struct Hand {
+    cards: HandType,
+    bid: i32,
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let lines = include_str!("../../test2.txt")
+        .lines()
+        .collect::<Vec<&str>>();
+
+    dbg!(&lines);
+
+    let hands: HashMap<String, Hand> = lines
+        .iter()
+        .map(|line| {
+            // println!("{:?}", &line.split(" ").collect::<Vec<_>>());
+            // dbg!(&line);
+            if let [first, second, ..] = &*line.split_whitespace().collect::<Vec<_>>() {
+                let cards = parse_handtype(first.to_string());
+                let bid = second.to_string().parse::<i32>().unwrap();
+
+                dbg!(&cards);
+
+                return (first.to_string(), Hand { cards, bid });
+            } else {
+                panic!("aahhhh");
+            }
+        })
+        .collect();
+
+    println!("{:?}", hands);
+}
+
+fn parse_handtype(str: String) -> HandType {
+    dbg!(&str);
+
+    let counts = get_counts(&str);
+
+    println!("{:?}", counts);
+
+    let counts_vec = counts
+        .iter()
+        .sorted_by(|a, b| b.1.cmp(a.1))
+        .collect::<Vec<_>>();
+
+    println!("{:?}", counts_vec);
+
+    match counts_vec.len() {
+        1 => {
+            let c = counts_vec[0].0;
+            match Card::from_char(c) {
+                Some(card) => HandType::Five(card),
+                None => panic!("AAhh"),
+            }
+        }
+        2 => {
+            let c1 = counts_vec[0].0;
+            let c2 = counts_vec[0].0;
+            match (Card::from_char(c1), Card::from_char(c2)) {
+                (Some(card1), Some(card2)) => HandType::Five(card1),
+                _ => panic!("AAhh"),
+            }
+        }
+        _ => HandType::Five(Card::A),
+    }
+}
+
+fn get_counts(str: &String) -> HashMap<char, i32> {
+    let mut counts: HashMap<char, i32> = HashMap::new();
+    let mut current_char = None;
+    let mut current_count = 0;
+
+    for c in str.chars() {
+        match current_char {
+            Some(ch) if ch == c => current_count += 1,
+            Some(ch) => {
+                *counts.entry(ch).or_insert(0) += current_count;
+                current_char = Some(c);
+                current_count = 1;
+            }
+            None => {
+                current_char = Some(c);
+                current_count = 1;
+            }
+        }
+    }
+
+    if let Some(ch) = current_char {
+        *counts.entry(ch).or_insert(0) += current_count;
+    }
+
+    return counts;
 }
